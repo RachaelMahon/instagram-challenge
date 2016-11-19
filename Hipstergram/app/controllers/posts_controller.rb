b25830734c880a88fd4e435d9038aa1e6cb6dbb2
RachaelMahon <rachmahon@gmail.com>
2016-11-19 22:08:03 +0000

    Some bootstrap added, show routes added

--- Hipstergram/app/controllers/posts_controller.rb
+++ Hipstergram/app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-
+    @posts = Post.all
   end
 
   def new
@@ -9,6 +9,11 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
 private
@@ -16,7 +21,5 @@ private
   def post_params
     params.require(:post).permit(:image, :caption)
   end
-
-end
 
 end
