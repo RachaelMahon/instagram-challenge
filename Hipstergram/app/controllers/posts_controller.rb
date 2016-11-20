cefc8132861080ef2c8e9d9f3fef4a6a7a09e1f8
RachaelMahon <rachmahon@gmail.com>
2016-11-20 00:28:55 +0000

    Current user added and joined to posts

--- Hipstergram/app/controllers/posts_controller.rb
+++ Hipstergram/app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
   def index
     @posts = Post.all
+  end
+
+  def show
   end
 
   def new
@@ -8,18 +14,41 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      # flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      # flash.now[:alert] = "Your post was not created!"
+      render :new
+    end
   end
 
-  def show
-    @post = Post.find(params[:id])
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      # flash[:success] = "Post updated"
+      redirect_to(post_path(@post))
+    else
+      # flash[:alert] = "Post not updated!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
 private
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
